af1605dfbc4c6486a7c74b611a0cc9353604da7a
jerdap17 <jeremy.dapaah@gmail.com>
2021-01-02 00:49:38 -0500

    Remove duplicated

--- 2019/d12.py
+++
@@ -1,8 +0,0 @@
-a = 0
-with open('/Users/jeremy/AdventOfCode/Input/d1.txt') as file:
-    for line in file:
-        b = int(line)
-        while b:
-            b = max(0, b // 3 - 2)
-            a += b
-print(a)
